5ea14643918e6fc295f159b8f179f8dbca93b9f3
maxmin <maxmin@webphp1.maxmin.it>
2021-05-06 13:44:27 +0100

    Recaptcha test keys

--- amazon/credential/recaptcha.sh
+++ amazon/credential/recaptcha.sh
@@ -4,5 +4,5 @@
 # for the key to work via the ELB domain name
 # you need to add amazonaws.com to the domains list
 
-RECAPTCHA_PRIVATE_KEY='<your_recaptcha_private_key>'
-RECAPTCHA_PUBLIC_KEY='<your_recaptcha_public_key>'
+RECAPTCHA_PRIVATE_KEY='6Lcam8gaAAAAABN2wuJGM9Y2gJwbMK4OgIMXf2Ki'
+RECAPTCHA_PUBLIC_KEY='6Lcam8gaAAAAAJEiMxTHraaIfZy5YSa-b4Zt30uU'
